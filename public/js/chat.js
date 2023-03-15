ee681b1d839ca8b2fd7c8ade043b71f40bfbfe99
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2023-03-15 00:50:44 -0600

    validate jwt in basic front

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,30 @@
-const socket = io();
+// validate if JWT is correct
+let user = null;
+let socket = null;
+const url = `${window.location.origin}/api/auth/`;
+
+// validate token in localStorage
+const validateJWT = async () => {
+  const token = localStorage.getItem('token') || '';
+
+  if (token.length <= 10) {
+    window.location = 'index.html';
+    throw new Error('No token in server');
+  }
+
+  const resp = await fetch(url, {
+    headers: { 'x-token': token },
+  });
+
+  const { user: userDB, token: tokenDB } = await resp.json();
+  localStorage.setItem('token', tokenDB);
+  user = userDB;
+};
+
+const main = async () => {
+  await validateJWT();
+};
+
+main();
+
+// const socket = io();
